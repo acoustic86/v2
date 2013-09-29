03e9c17b2e649bf0087db3dfc0502ec47383931c
Alex Kozlov <arcadiabakery@gmail.com>
2013-09-29 15:57:06 -0700

    other profuction environment changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,14 +22,14 @@ Showping::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = true                                   #changed to deploy to Heroku
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true                                        #changed to deploy to Heroku
 
   # Generate digests for assets URLs.
   config.assets.digest = true
