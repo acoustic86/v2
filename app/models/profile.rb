a77bdc5c6f0dfb21c42c79e2dcac01862ff9dc31
Alex Kozlov <arcadiabakery@gmail.com>
2013-09-29 17:04:25 -0700

    removed sunspot searchable from profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,9 +5,9 @@ class Profile < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "250x250>" }, :default_url => "/images/:style/No_Image.png"
   paginates_per 50
   
-  searchable do
-    text :name, :show_type, :latitude, :longitude    
-  end
+  #searchable do
+    #text :name, :show_type, :latitude, :longitude    
+  #end
   
   geocoded_by :current_location
   after_validation :geocode, :if => :current_location_changed?
