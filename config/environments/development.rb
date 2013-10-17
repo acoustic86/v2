186377bf4ad1a9ce417220732ce37ab5882e6cdb
acoustic86 <arcadiabakery@gmail.com>
2013-10-16 20:46:52 -0700

    mailer setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,22 @@
 Showping::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
-  config.action_mailer.default_url_options = { :host => '127.0.0.1:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:8080' }
+  # Custom Mailer Settings
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  
+  APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+  
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'ladosha.com',
+  :user_name            => APP_CONFIG['email'],
+  :password             => APP_CONFIG['password'],
+  :authentication       => 'login',
+  :enable_starttls_auto => true  }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -16,7 +31,7 @@ Showping::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -43,4 +58,5 @@ Showping::Application.configure do
     #:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   #}
 #}
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
   config.mailer_sender = "arcadiabakery@gmail.com"
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = "Devise::Mailer"
+  config.mailer = "Devise::Mailer" #uncommented
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
@@ -111,7 +111,7 @@ Devise.setup do |config|
   config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
-  # config.confirmation_keys = [ :email ]
+  config.confirmation_keys = [ :email ] #uncommented
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
