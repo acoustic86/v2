45c0fd327d5d81be3d2395ab07fcd0b494ea2d0e
acoustic86 <arcadiabakery@gmail.com>
2013-11-24 15:16:26 -0700

    2 removed reference to application yml file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Showping::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
   
-  APP_CONFIG = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]
+  #APP_CONFIG = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]
   
   config.action_mailer.smtp_settings = {
   :address => "smtp.gmail.com",
