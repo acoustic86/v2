a688a347764c0f3fb52151ccf9ab5c80c53f07e6
acoustic86 <arcadiabakery@gmail.com>
2014-03-14 22:11:22 -0700

    config assets debug true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Showping::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false #changed for faster page loading in development.... change back before deploying
+  config.assets.debug = true #changed for faster page loading in development.... change back before deploying
   
   Paperclip.options[:command_path] = "C:\ImageMagic"
   Paperclip.options[:swallow_stderr] = false
